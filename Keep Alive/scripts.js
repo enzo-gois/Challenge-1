e8e480929d208d3c7db17587e4a42e6f18c4c0f2
Enzo Gois <enzoab26@outlook.com>
2024-03-15 15:36:04 -0300

    adicionando relogio funcional

--- Keep Alive/scripts.js
+++ Keep Alive/scripts.js
@@ -1,9 +1,17 @@
 window.addEventListener('load', function() {
   startTimer()
+  updateClock()
 })
 
 let countdown = document.getElementById("time")
 let remainingTime = 30
+
+function updateClock() {
+  const date = new Date()
+  const hours = date.getHours().toString().padStart(2, '0')
+  const minutes = date.getMinutes().toString().padStart(2,'0')
+  clock.textContent = `${hours}:${minutes}`
+}
 
 function startTimer() {
   setInterval(function() {
